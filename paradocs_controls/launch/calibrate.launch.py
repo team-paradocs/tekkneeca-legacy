21e3cc585ed5335bd477475d01ae662e5376b4da
liver121888 <liver121888@gmail.com>
2024-02-28 00:07:27 -0500

    Add camera calib pipeline.

---
+++ paradocs_controls/launch/calibrate.launch.py
@@ -0,0 +1,98 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.conditions import LaunchConfigurationEquals
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+
+from easy_handeye2.common_launch import arg_calibration_type, arg_tracking_base_frame, arg_tracking_marker_frame, arg_robot_base_frame, \
+    arg_robot_effector_frame
+
+def launch_setup(context, *args, **kwargs):
+
+    arg_name = DeclareLaunchArgument('name')
+
+    node_dummy_calib_eih = Node(package='tf2_ros', executable='static_transform_publisher', name='dummy_publisher',
+                                condition=LaunchConfigurationEquals('calibration_type', 'eye_in_hand'),
+                                arguments=f'--x 0 --y 0 --z 0.1 --qx 0 --qy 0 --qz 0 --qw 1'.split(' ') + ['--frame-id', LaunchConfiguration('robot_effector_frame'),
+                                                                                                           '--child-frame-id', LaunchConfiguration('tracking_base_frame')])
+
+    node_dummy_calib_eob = Node(package='tf2_ros', executable='static_transform_publisher', name='dummy_publisher',
+                                condition=LaunchConfigurationEquals('calibration_type', 'eye_on_base'),
+                                arguments=f'--x 1 --y 0 --z 0 --qx 0 --qy 0 --qz 0 --qw 1'.split(' ') + ['--frame-id', LaunchConfiguration('robot_base_frame'),
+                                                                                                         '--child-frame-id', LaunchConfiguration('tracking_base_frame')])
+
+    handeye_server = Node(package='easy_handeye2', executable='handeye_server', name='handeye_server', parameters=[{
+        'name': LaunchConfiguration('name'),
+        'calibration_type': LaunchConfiguration('calibration_type'),
+        'tracking_base_frame': LaunchConfiguration('tracking_base_frame'),
+        'tracking_marker_frame': LaunchConfiguration('tracking_marker_frame'),
+        'robot_base_frame': LaunchConfiguration('robot_base_frame'),
+        'robot_effector_frame': LaunchConfiguration('robot_effector_frame'),
+    }])
+
+    handeye_rqt_calibrator = Node(package='easy_handeye2', executable='rqt_calibrator.py',
+                                  name='handeye_rqt_calibrator',
+                                  arguments=['--ros-args', '--log-level', 'debug'],
+                                  parameters=[{
+                                      'name': LaunchConfiguration('name'),
+                                      'calibration_type': LaunchConfiguration('calibration_type'),
+                                      'tracking_base_frame': LaunchConfiguration('tracking_base_frame'),
+                                      'tracking_marker_frame': LaunchConfiguration('tracking_marker_frame'),
+                                      'robot_base_frame': LaunchConfiguration('robot_base_frame'),
+                                      'robot_effector_frame': LaunchConfiguration('robot_effector_frame'),
+                                  }])
+
+    return [
+        arg_name,
+        arg_calibration_type,
+        arg_tracking_base_frame,
+        arg_tracking_marker_frame,
+        arg_robot_base_frame,
+        arg_robot_effector_frame,
+        node_dummy_calib_eih,
+        node_dummy_calib_eob,
+        handeye_server,
+        handeye_rqt_calibrator,
+    ]
+
+
+
+def generate_launch_description():
+
+    declared_arguments = [
+        DeclareLaunchArgument(
+            'name',
+            default_value='eih_cam1',
+            description='',
+        ),
+        DeclareLaunchArgument(
+            'calibration_type',
+            default_value='eye_in_hand',
+            description='',
+        ),
+        DeclareLaunchArgument(
+            'tracking_base_frame',
+            default_value='camera_link',
+            description='',
+        ),
+        DeclareLaunchArgument(
+            'tracking_marker_frame',
+            default_value='marker_1_frame',
+            description='',
+        ),
+        DeclareLaunchArgument(
+            'robot_base_frame',
+            default_value='lbr/link_0',
+            description='',
+        ),
+        DeclareLaunchArgument(
+            'robot_effector_frame',
+            default_value='lbr/link_ee',
+            description='',
+        ),
+    ]
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
+
+
+
---
+++ paradocs_controls/launch/single.launch.py
@@ -0,0 +1,78 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch.utilities import perform_substitutions
+from launch_ros.actions import Node
+
+
+def launch_setup(context, *args, **kwargs):
+
+    # eye = perform_substitutions(context, [LaunchConfiguration('eye')])
+
+    aruco_single_params = {
+        'image_is_rectified': True,
+        'marker_size': LaunchConfiguration('marker_size'),
+        'marker_id': LaunchConfiguration('marker_id'),
+        'reference_frame': '',
+        'camera_frame': LaunchConfiguration('camera_frame'),
+        'marker_frame': LaunchConfiguration('marker_frame'),
+        'corner_refinement': LaunchConfiguration('corner_refinement'),
+    }
+
+    aruco_single = Node(
+        package='aruco_ros',
+        executable='single',
+        parameters=[aruco_single_params],
+        remappings=[('/camera_info', '/camera/color/camera_info'),
+                    ('/image', '/camera/color/image_rect_raw')],
+    )
+
+    return [aruco_single]
+
+
+def generate_launch_description():
+
+    marker_id_arg = DeclareLaunchArgument(
+        'marker_id', default_value='33',
+        description='Marker ID. '
+    )
+
+    marker_size_arg = DeclareLaunchArgument(
+        'marker_size', default_value='0.04',
+        description='Marker size in m. '
+    )
+
+    camera_frame_arg = DeclareLaunchArgument(
+        'camera_frame', default_value='camera_color_frame',
+        description='Camera frame.',
+    )
+
+    marker_frame_arg = DeclareLaunchArgument(
+        'marker_frame', default_value='marker_1_frame',
+        description='Frame in which the marker pose will be refered. '
+    )
+
+    # reference_frame = DeclareLaunchArgument(
+    #     'reference_frame', default_value='lbr/ee_link',
+    #     description='Reference frame. '
+    #     'Leave it empty and the pose will be published wrt param parent_name. '
+    # )
+
+    corner_refinement_arg = DeclareLaunchArgument(
+        'corner_refinement', default_value='LINES',
+        description='Corner Refinement. ',
+        choices=['NONE', 'HARRIS', 'LINES', 'SUBPIX'],
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    ld.add_action(marker_id_arg)
+    ld.add_action(marker_size_arg)
+    ld.add_action(camera_frame_arg)
+    ld.add_action(marker_frame_arg)
+    # ld.add_action(reference_frame)
+    ld.add_action(corner_refinement_arg)
+    ld.add_action(OpaqueFunction(function=launch_setup))
+
+    return ld
